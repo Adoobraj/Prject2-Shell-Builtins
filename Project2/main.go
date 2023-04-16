9a0b0ae93f86c3f0ade42de691e64a920fa3deb5
Ricky Gibbons <105470662+Cookiefied@users.noreply.github.com>
2023-04-16 15:36:40 -0500

    mkdir command from Thomas (trg0117)
    Added the functionality to support my mkdir command

--- Project2/main.go
+++ Project2/main.go
@@ -80,6 +80,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return builtins.EnvironmentVariables(w, args...)
 	case "pwd":
 		return builtins.PrintWorkingDirectory(w)
+	case "mkdir":
+		return builtins.Mkdir(args...)
 	case "exit":
 		exit <- struct{}{}
 		return nil
