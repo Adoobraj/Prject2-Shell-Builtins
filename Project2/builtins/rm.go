e2b94bdf7b233dc03cb309690a8cb27b5106e6f3
Michael Ogunsanlu <119998317+michaelToby@users.noreply.github.com>
2023-05-09 23:41:32 -0500

    Update rm.go

--- Project2/builtins/rm.go
+++ Project2/builtins/rm.go
@@ -1,21 +1,18 @@
 package builtins
 
 import (
-    "fmt"
-    "os"
+   "fmt"
+   "os"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("usage: rm [file or directory]")
-        os.Exit(1)
-    }
-
-    path := os.Args[1]
-
-    err := os.RemoveAll(path)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+   // Delete the file named "file.txt".
+   err := os.Remove("file.txt")
+   if err != nil {
+       fmt.Println(err)
+       return
+   }
+   fmt.Println("File successfully deleted")
 }
+
+ 
